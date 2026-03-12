3cd982dcfe0c78d08b735fbd1cc80b360db63861
EmreCandemiir <emrecan0_0@icloud.com>
2026-03-13 00:52:30 +0300

    Initial commit from reactProj

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,34 @@
+/*import TaskShow from "./TaskShow";
+
+function TaskList({ tasks ,onDelete}) {
+  return (
+    <div className='task-list'>
+      {tasks.map((task, index) => {
+        return <TaskShow key={index} task={task} onDelete={onDelete} onUpdate={onUpdate}/>;
+      })}
+    </div>
+  );
+}
+
+export default TaskList; */
+
+import TaskShow from './TaskShow';
+
+function TaskList({ tasks, onDelete, onUpdate }) {
+  return (
+    <div className="task-list">
+      {tasks.map((task, index) => {
+        return (
+          <TaskShow
+            key={index}
+            task={task}
+            onDelete={onDelete}
+            onUpdate={onUpdate}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default TaskList;
